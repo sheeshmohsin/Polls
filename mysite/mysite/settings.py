460d2062a96b6da8508e651215a1864486c14cb1
Sheesh Mohsin <sheeshmohsin@gmail.com>
2014-04-25 17:45:02 +0530

    Written first view

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -48,6 +48,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
 ROOT_URLCONF = 'mysite.urls'
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
